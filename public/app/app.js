57c9b0af52baf6d3d99bd9e91f54e40791d92d16
imhotepp <stefanescu.dragos@gmail.com>
2014-10-07 19:02:12 +0000

    getting data from remote mongo with api and ngResource

--- public/app/app.js
+++ public/app/app.js
@@ -1,12 +1,16 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['ngResource']);
 
-app.controller('testCtrl', function($scope) {
-    $scope.jobs = [{
+app.controller('testCtrl', function($scope,$resource) {
+    $scope.jobs = $resource('/api/jobs').query();
+   
+   /* 
+    [{
         title: 'Angular developer',
         description: 'a veri nice to be doing thing'
     }, {
         title: 'MVC developer',
         description: 'interesting and fast serverside fwk to create staff'
     }];
+    */
 
 });
